9501382b148c3a891a8282cad5f6cf99bbb2b96a
Alessio Libbi <alex.libbi@gmail.com>
2023-06-13 17:16:50 +0200

    Add:exercise

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from "axios";
+import Project from "./components/Project.vue";
+
+export default {
+  data() {
+    return {
+      Api: "http://127.0.0.1:8000",
+      projects: [],
+    };
+  },
+  components: {
+    Project,
+  },
+  mounted() {
+    this.getCard();
+  },
+  methods: {
+    getCard() {
+      axios.get(`${this.Api}/api/projects`).then((resp) => {
+        this.projects = resp.data.results;
+      });
+    },
+  },
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1 class="text-center mt-5 mb-5">I Nostri Progetti</h1>
+    <div class="row row-cols-3 g-3">
+      <div
+        class="col d-flex justify-content-center"
+        v-for="project in projects"
+        :key="project.id"
+      >
+        <Project :project="project" />
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use "./style/general.scss";
 </style>
---
+++ src/components/Project.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "Project",
+  props: {
+    project: Object,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="card h-100" style="width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">{{ project.title }}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">
+        {{ project.slug }}
+      </h6>
+      <p class="card-text">
+        {{ project.description }}
+      </p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
